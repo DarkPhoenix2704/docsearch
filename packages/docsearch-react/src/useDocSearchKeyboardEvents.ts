84b77037991f54d0cae27bf5cd634c0c37ac3242
DarkPhoenix2704 <anbarasun123@gmail.com>
2023-09-28 15:44:43 +0530

    fix: remove keyboard events

--- packages/docsearch-react/src/useDocSearchKeyboardEvents.ts
+++ packages/docsearch-react/src/useDocSearchKeyboardEvents.ts
@@ -8,18 +8,6 @@ export interface UseDocSearchKeyboardEventsProps {
   searchButtonRef?: React.RefObject<HTMLButtonElement>;
 }
 
-function isEditingContent(event: KeyboardEvent): boolean {
-  const element = event.target as HTMLElement;
-  const tagName = element.tagName;
-
-  return (
-    element.isContentEditable ||
-    tagName === 'INPUT' ||
-    tagName === 'SELECT' ||
-    tagName === 'TEXTAREA'
-  );
-}
-
 export function useDocSearchKeyboardEvents({
   isOpen,
   onOpen,
@@ -27,51 +15,11 @@ export function useDocSearchKeyboardEvents({
   onInput,
   searchButtonRef,
 }: UseDocSearchKeyboardEventsProps) {
-  React.useEffect(() => {
-    function onKeyDown(event: KeyboardEvent) {
-      function open() {
-        // We check that no other DocSearch modal is showing before opening
-        // another one.
-        if (!document.body.classList.contains('DocSearch--active')) {
-          onOpen();
-        }
-      }
-      if (
-        (event.keyCode === 27 && isOpen) ||
-        // The `Cmd+K` shortcut both opens and closes the modal.
-        // We need to check for `event.key` because it can be `undefined` with
-        // Chrome's autofill feature.
-        // See https://github.com/paperjs/paper.js/issues/1398
-        (event.key?.toLowerCase() === 'k' &&
-          (event.metaKey || event.ctrlKey)) ||
-        // The `/` shortcut opens but doesn't close the modal because it's
-        // a character.
-        (!isEditingContent(event) && event.key === '/' && !isOpen)
-      ) {
-        event.preventDefault();
-
-        if (isOpen) {
-          onClose();
-        } else if (!document.body.classList.contains('DocSearch--active')) {
-          open();
-        }
-      }
-
-      if (
-        searchButtonRef &&
-        searchButtonRef.current === document.activeElement &&
-        onInput
-      ) {
-        if (/[a-zA-Z0-9]/.test(String.fromCharCode(event.keyCode))) {
-          onInput(event);
-        }
-      }
-    }
-
-    window.addEventListener('keydown', onKeyDown);
-
-    return () => {
-      window.removeEventListener('keydown', onKeyDown);
-    };
-  }, [isOpen, onOpen, onClose, onInput, searchButtonRef]);
+  React.useEffect(() => {}, [
+    isOpen,
+    onOpen,
+    onClose,
+    onInput,
+    searchButtonRef,
+  ]);
 }
